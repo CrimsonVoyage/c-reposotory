4831908500fe0b82b9501d1a9d50f6504b789f71
SenpaiOfWeb <74235676+SenpaiOfWeb@users.noreply.github.com>
2020-12-16 19:24:20 +0530

    Add files via upload

---
+++ mycoding/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace mycoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             
+            int a = 10;
+            int b = 20;
+            int c = 30;
+            string linen = "I love programming on c#";
+            Console.WriteLine(value: a);
+            Console.WriteLine(value: b);
+            Console.WriteLine(value: c);
+            Console.WriteLine(value: a + b);
+            Console.WriteLine(value: b + c);
+            Console.WriteLine(value: c + a);
+            Console.WriteLine(value: a + b + c);
+            Console.WriteLine(linen);
+
+            Console.WriteLine("What is your first name?");
+
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("My first name is." + " " + firstname);
+
+            Console.WriteLine("What is your last name?");
+
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("My last name is." + " " + lastname);
+
+            int d = a - b;
+            int e = b - c;
+            int f = a * b;
+            int g = b * c;
+
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+
+            int h = a % b;
+            int i = b % c;
+
+            Console.WriteLine(h);
+            Console.WriteLine(i);
+
+            if (a < b) 
+            {
+                Console.WriteLine(a + " " + "is smaller then" + " " + b);
+            } else
+            {
+                Console.WriteLine("a is greater then b");
+            }
+
+            if (a > b)
+            {
+                Console.WriteLine("a is greater then b");
+            }
+            else
+            {
+                Console.WriteLine(a + " " + "is smaller then" + " " + b);
+            }
+
+            Console.WriteLine("Type a number!");
+
+            int day = int.Parse(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thrsday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+            }
+
+             */
+
+            /*
+             
+            Console.WriteLine("Input the first number!");
+
+            int j = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input the second number!");
+
+            int k = int.Parse(Console.ReadLine());
+
+            while (j <= k)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+
+             */
+
+            /*
+            
+            Console.WriteLine("input your first number!");
+
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input your second number!");
+
+            int b = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+            while (a <= b);
+
+            */
+
+            /*
+             
+            Console.WriteLine("input your first number!");
+
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input your second number!");
+
+            int b = int.Parse(Console.ReadLine());
+
+            int i = 0;
+
+
+            for (i = a; a <= b;a++)
+            {
+                Console.WriteLine(a);
+            } 
+
+             */
+
+
+
+
+        }
+    }
+}
